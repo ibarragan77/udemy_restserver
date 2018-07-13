35e04b69a34d51daae3e636c46d0da574d553e2f
ibarragan <ibarragan@infraestructuragis.com>
2018-07-12 21:10:47 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,25 @@
 //=============================
 
 process.env.PORT = process.env.PORT || 8090;
+
+//==========
+//Entorno
+//=========
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//===============
+// Base de Datos
+//================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else {
+    urlDB = 'mongodb://cafe-user:ds133621@ds133621.mlab.com:33621/cafe';
+}
+
+process.env.URLDB = urlDB;
+
+
