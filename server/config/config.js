afb21adc0ad4a7b7b04638c3d0eb526a16fa2eea
ibarragan <ibarragan@infraestructuragis.com>
2018-07-12 21:29:22 -0500

    MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
-    urlDB = 'mongodb://cafe-user:ds133621@ds133621.mlab.com:33621/cafe';
+    urlDB =  processe.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
